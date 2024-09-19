447b6425599c334e4591e2e52e1e77838a254dc4
Amir Hossein Habibi <habibi.dev@gmail.com>
2024-09-19 22:46:46 +0330

    Update MusicRepository.ts

--- src/repository/MusicRepository.ts
+++ src/repository/MusicRepository.ts
@@ -2,14 +2,12 @@ import pkg from "lodash";
 import fs from "fs";
 
 export default class MusicRepository {
-    private musicFolder = '/musik/';
     private files: string[] = [];
 
     getMusics() {
-        const baseDir = pkg.get(process, "env.DIR", "./files") as string
-        const directory = baseDir + this.musicFolder
+        const baseDir = pkg.get(process, "env.DIR", "files") as string + "/"
 
-        this.files = fs.readdirSync(directory).map(string => directory + string);
+        this.files = fs.readdirSync(baseDir).map(string => baseDir + string);
 
         return this.files;
     }
