f263364eeb95c83a2bd8cf2c1693eee64cc4353f
Amir Hossein Habibi <habibi.dev@gmail.com>
2024-09-27 20:04:06 +0330

    Create ServerInterface.ts

---
+++ src/interfaces/ServerInterface.ts
@@ -0,0 +1,13 @@
+export interface ServerInterface {
+    "stream_key": string,
+    "url_rtmp": string,
+    "dir": string,
+    "media_type": "video" | "audio",
+    "cover": string,
+    "audio_codec": string,
+    "audio_sample_rate": number,
+    "audio_channels": 1 | 2,
+    "audio_bitrate": string,
+    "scale": string,
+    "ignore_directories": string[]
+}
