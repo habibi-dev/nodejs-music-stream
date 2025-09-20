7b6814108911253ab97a77e5956f46be227da8d0
Amir Hossein Habibi <habibi.dev@gmail.com>
2025-09-21 00:27:18 +0330

    ♻️ refactor(stream/view): Use path.resolve for views path to ensure correct resolution

--- src/modules/stream/config/StreamViewConfig.ts
+++ src/modules/stream/config/StreamViewConfig.ts
@@ -1,8 +1,8 @@
-﻿import path from "path";
+import path from "path";
 import {Express} from "express";
 
 export default function StreamViewConfig(app: Express): void {
-    const viewsPath = path.join(process.cwd(), "views");
+    const viewsPath = path.resolve(__dirname, "..", "views");
     app.set("views", viewsPath);
     app.set("view engine", "ejs");
 }
